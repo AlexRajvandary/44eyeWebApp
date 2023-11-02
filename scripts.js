6bca02dc26258bc29e53b99b2b35a3e0aa4332af
Alex <stark.11raj@gmail.com>
2023-11-02 13:46:27 +0000

    MainButton text changes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,17 @@ let mainBtn = window.Telegram.WebApp.MainButton;
 let backBtn = window.Telegram.WebApp.BackButton;
 let webApp = window.Telegram.WebApp;
 
-const Product = require('./product.js');
+class Product {
+    constructor(id, name, description, category, price, image, season, gender) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.season = season;
+        this.gender = gender;
+    }
+}
 
 const products = [
     new Product(1, "Кроссовки Nike Белые", "обувь", "женское", "весна", "sneakers/1.jpg", 7990, "Описание товара 1"),
--- scripts/product.js
+++ scripts/product.js
@@ -10,4 +10,4 @@ class Product {
     }
 }
 
-module.exports = Product;
+//module.exports = Product;
