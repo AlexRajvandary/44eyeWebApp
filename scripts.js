9f693181d0a1b021bc736761102b2d9bc208531a
Alex <stark.11raj@gmail.com>
2023-11-14 01:33:48 +0000

    Корзина стилизована

--- scripts.js
+++ scripts.js
@@ -447,7 +447,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function mainBtnClicked(){
         $('.catalogue').hide();
         mainBtn.text = "Оформить заказ";
-        displayCartProducts(cart);
+        /*displayCartProducts(cart);*/
         $('.order_view').show();
         backBtn.show();
     }
