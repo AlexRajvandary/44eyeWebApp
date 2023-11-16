be1661fd9993666ed01d6f45285f0bd5ff140089
Alex <stark.11raj@gmail.com>
2023-11-16 03:10:32 +0000

    Добавлена кнопка удалить для корзины

--- scripts.js
+++ scripts.js
@@ -331,7 +331,8 @@ function toggleCart(orderCard, productId, productContainer, btn) {
             const colorSelect = createSelect(orderItem.product.colors, orderItem.selectedColor, listItem);
 
             const deleteButton = document.createElement('button');
-                deleteButton.classList.add("fa fa-trash-o");
+                deleteButton.classList.add("fa");
+                deleteButton.classList.add("fa-trash-o");
                 deleteButton.ariaHidden = 'true';
                 deleteButton.addEventListener('click', function() {
                 cart.remove(productId, orderItem);
